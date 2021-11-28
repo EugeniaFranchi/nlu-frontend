faaf57d7801823a4d264703e68f6c190f2ade397
Manuel <75450615+vlk-yr@users.noreply.github.com>
2021-11-28 19:13:23 -0300

    cambios_variable_de_entorno

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,9 @@
 import { createStore, combineReducers } from "redux";
-import { nluReducer } from "./reducers/NluReducer";
-import { stateReducer } from "./reducers/StateReducer";
+import { editorReducer } from "./reducers/EditorReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 const reducer = combineReducers({
-    nlu: nluReducer,
-    state: stateReducer
+    editor: editorReducer
   })
 
 export default createStore(reducer, composeWithDevTools());
